cc973b2a90d5bbd25533b00354f0062f9833f930
Enky-create <komkowvadim@gmail.com>
2020-02-10 22:17:54 +0700

    ÐДобавляет новых волшебников

---
+++ js/setup.js
@@ -0,0 +1,45 @@
+'use strict';
+var WIZARDS_QUANTITY = 4;
+
+var showSetup = document.querySelector('.setup');
+showSetup.classList.remove('hidden');
+
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandomInRange = function (array) {
+  var min = 0;
+  var max = array.length - 1;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var randomWizard = function (name, surname, coatColor, eyesColor) {
+  var wizard = {
+    name: names[getRandomInRange(name)],
+    surname: surnames[getRandomInRange(surname)],
+    coatColor: coatColors[getRandomInRange(coatColor)],
+    eyesColor: eyesColors[getRandomInRange(eyesColor)]
+  };
+  return wizard;
+};
+
+var wizardsInHtml = function (similarWizard, fragment, counterOfWizards) {
+  for (var i = 0; i < counterOfWizards; i++) {
+    var wizard = randomWizard(names, surnames, coatColors, eyesColors);
+    var wizardElement = similarWizard.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name + ' ' + wizard.surname;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    fragment.appendChild(wizardElement);
+  }
+  return fragment;
+};
+
+var similarWizard = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var fragment = document.createDocumentFragment();
+var listOfWizards = document.querySelector('.setup-similar-list');
+listOfWizards.appendChild(wizardsInHtml(similarWizard, fragment, WIZARDS_QUANTITY));
+var listContainer = document.querySelector('.setup-similar');
+listContainer.classList.remove('hidden');
